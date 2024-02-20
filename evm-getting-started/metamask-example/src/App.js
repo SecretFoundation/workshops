c76d42ce88f378b0d1336c838e70d1bb69166195
writersblockchain <seanmconrad94@gmail.com>
2024-02-20 14:06:49 -0500

    frontend

--- evm-getting-started/metamask-example/src/App.js
+++ evm-getting-started/metamask-example/src/App.js
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { SecretNetworkClient, MetaMaskWallet } from "secretjs";
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
+  const [count, setCount] = useState(0);
 
-  // your codeHash to go here
   let codeHash = "";
-  //your contractAddress to go here
   let contractAddress = "";
 
   const connectWallet = async () => {
@@ -15,16 +14,13 @@ function App() {
       const [ethAddress] = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
-
       const wallet = await MetaMaskWallet.create(window.ethereum, ethAddress);
-
       const secretjs = new SecretNetworkClient({
         url: "https://api.pulsar3.scrttestnet.com",
         chainId: "pulsar-3",
         wallet: wallet,
         walletAddress: wallet.address,
       });
-
       console.log("Connected to Secret Network", secretjs);
       setIsConnected(true);
     } catch (error) {
@@ -33,62 +29,80 @@ function App() {
   };
 
   const increaseCount = async () => {
-    const [ethAddress] = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
+    try {
+      const [ethAddress] = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const wallet = await MetaMaskWallet.create(window.ethereum, ethAddress);
+      const secretjs = new SecretNetworkClient({
+        url: "https://api.pulsar3.scrttestnet.com",
+        chainId: "pulsar-3",
+        wallet: wallet,
+        walletAddress: wallet.address,
+      });
 
-    const wallet = await MetaMaskWallet.create(window.ethereum, ethAddress);
-
-    const secretjs = new SecretNetworkClient({
-      url: "https://api.pulsar3.scrttestnet.com",
-      chainId: "pulsar-3",
-      wallet: wallet,
-      walletAddress: wallet.address,
-    });
-
-    const tx = await secretjs.tx.compute.executeContract(
-      {
-        sender: wallet.address,
-        contract_address: contractAddress,
-        msg: {
-          increment: {},
+      const tx = await secretjs.tx.compute.executeContract(
+        {
+          sender: wallet.address,
+          contract_address: contractAddress,
+          msg: {
+            increment: {},
+          },
+          code_hash: codeHash,
         },
-        code_hash: codeHash,
-      },
-      { gasLimit: 100_000 }
-    );
+        { gasLimit: 100_000 }
+      );
+      console.log(tx);
 
-    console.log(tx);
+      queryCount();
+    } catch (error) {
+      console.error("Error on increaseCount", error);
+    }
   };
 
   const queryCount = async () => {
-    const [ethAddress] = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
+    try {
+      const [ethAddress] = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const wallet = await MetaMaskWallet.create(window.ethereum, ethAddress);
+      const secretjs = new SecretNetworkClient({
+        url: "https://api.pulsar3.scrttestnet.com",
+        chainId: "pulsar-3",
+        wallet: wallet,
+        walletAddress: wallet.address,
+      });
 
-    const wallet = await MetaMaskWallet.create(window.ethereum, ethAddress);
-    const secretjs = new SecretNetworkClient({
-      url: "https://api.pulsar3.scrttestnet.com",
-      chainId: "pulsar-3",
-      wallet: wallet,
-      walletAddress: wallet.address,
-    });
+      let my_query = await secretjs.query.compute.queryContract({
+        contract_address: contractAddress,
+        code_hash: codeHash,
+        query: { get_count: {} },
+      });
 
-    let my_query = await secretjs.query.compute.queryContract({
-      contract_address: contractAddress,
-      code_hash: codeHash,
-      query: { get_count: {} },
-    });
+      setCount(my_query.count);
+    } catch (error) {
+      console.error("Error on queryCount", error);
+    }
   };
+
+  useEffect(() => {
+    if (isConnected) {
+      queryCount();
+    }
+  }, [isConnected]); // Query count when the wallet connects
 
   return (
     <div className="App">
       <header className="App-header">
+        <h5>Current Count: {count}</h5>
         <button onClick={connectWallet} disabled={isConnected}>
           {isConnected ? "Connected" : "Connect Wallet"}
         </button>
+        <br></br>
+        {/* <button onClick={queryCount}>Query Count</button> */}
+
         <button onClick={increaseCount}>Increase Count</button>
-        <button onClick={queryCount}>Query Count</button>
+        <br></br>
       </header>
     </div>
   );
